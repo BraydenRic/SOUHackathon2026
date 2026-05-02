07f7cd3cee2dbd1bd9c7eb216b426163bafa9d94
Alec Clark <aclark122101@gmail.com>
2026-05-02 12:05:59 -0700

    Fixed all naming and updated readme

--- FantasyTrader/src/components/layout/Navbar.tsx
+++ FantasyTrader/src/components/layout/Navbar.tsx
@@ -29,7 +29,7 @@ export function Navbar() {
           <polyline points="22 7 13.5 15.5 8.5 10.5 2 17" />
           <polyline points="16 7 22 7 22 13" />
         </svg>
-        StockDraft
+        Fantasy Trader
       </Link>
 
       <div className="flex items-center gap-2">
